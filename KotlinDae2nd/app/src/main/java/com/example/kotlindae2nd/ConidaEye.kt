385c4ea482bea15455a4a17ea6c6b23bfd0ad539
okary6684 <112407465+okary6684@users.noreply.github.com>
2023-02-21 21:06:25 +0900

    コニダエ開眼

---
+++ KotlinDae2nd/app/src/main/java/com/example/kotlindae2nd/ConidaEye.kt
@@ -0,0 +1,128 @@
+package com.example.kotlindae2nd
+
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class ConidaEye(nwContext : Activity) {
+    private var nowContext = nwContext
+    private var imageCapture: ImageCapture? = null
+    private var cameraExecutor: ExecutorService
+
+    init{
+        checkPermission()
+        startCamera()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(nowContext)
+
+        cameraProviderFuture.addListener({
+            // ライフサイクルにバインドするために利用する
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            imageCapture = ImageCapture.Builder().build()
+
+
+            // 「インカメ」を設定
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+                // バインドされているカメラの解除
+                cameraProvider.unbindAll()
+
+                // カメラをライフサイクルにリバインド
+                cameraProvider.bindToLifecycle(nowContext as LifecycleOwner,cameraSelector,imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(nowContext))}
+
+    private fun checkPermission(){
+        if (ActivityCompat.checkSelfPermission(
+                nowContext,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                nowContext,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+    companion object{
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).toTypedArray()
+    }
+
+    fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // 写真の名前の設定（タイムスタンプ）とMediaStoreの設定
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        //保存するオプションを作成
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(nowContext.contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // 画像のキャプチャ
+        // 結果はImageCapture.OnImageSavedCallbackのコールバックで返ってくる
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(nowContext),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun
+                        onImageSaved(output: ImageCapture.OutputFileResults){
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Log.d(TAG, msg)
+                }
+            }
+        )
+    }
+    fun finish(){
+        cameraExecutor.shutdown()
+    }
+
+}
